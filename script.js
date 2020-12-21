6ca6eb8e23f8179de731c4a12f3df990cd39f40f
Boubacar <diallobouba905@gmail.com>
2020-12-21 16:52:19 +0100

    ajout du jour

--- script.js
+++ script.js
@@ -80,3 +80,79 @@
 // document.write("le tableau est:"," ",tab+"<br>")
 // document.write("le nombre de valeur postive est:"," ",pos +"<br>")
 // document.write("le nombre de valeur negative est:"," ",neg);
+
+/*****exo 6.9 ******/
+
+// var tab=[4,8,9,10,7,12]
+// var som=0;
+// for(i=0; i<tab.length; i++ )
+// {
+//    som+=tab[i];
+// }
+// document.write(som)
+
+/****exo6.10 ******/
+
+// var tab1=[4,8,7,9,1,5,4,6],
+//     tab2=[7,6,5,2,1,3,7,4],
+//     tab3=[], 
+//     n=8
+// for(var i=0; i<n; i++)
+// {
+    
+    
+//        tab3[i]= tab1[i]+tab2[i];
+    
+// }
+// document.write(tab3)
+
+/*****exo 6.11 ******/
+
+// var tab1=[4,8,7,12]
+// var    tab2=[3,6]
+//  var  som=0;
+// for(var i=0,n=tab1.length; i<n; i++)
+// {
+//     for(var j=0,n1=tab2.length; j<n1; j++)
+//     {
+//         som+=tab1[i]*tab2[j];
+//     }
+// }
+// document.write(som)
+
+/****exo 6.12 ******/
+
+
+// var n=parseInt(window.prompt("entrer la taille du tableau"))
+// var tab= new Array(n)
+// for( var i=0; i<tab.length; i++)
+// {
+//      var tmp= window.prompt("entrer un nombre");
+     
+//      tab[i]=tmp;
+     
+//      tab[i] =parseInt(tab[i])+parseInt(1);
+    
+// }
+// document.write(tab)
+
+/***exo 6.13 ****/
+
+ var n=parseInt(window.prompt("entrer la taille du tableau"))
+var tab= new Array(n)
+var pgn=0;
+var posi=0;
+for( var i=0; i<tab.length; i++)
+{ 
+    
+    var tmp=window.prompt("entrer un nombre");
+    console.log(tmp)
+    if(tmp>pgn)
+    {
+        
+        posi=i;
+        pgn=tmp;
+    }
+   
+}
+document.write("le plus grand nombre est:"," ",pgn," ","et sa position est:"," ",posi)
